932e786ac29f8306989dc29e72f6abc1c65dd238
aliaffani07 <aliaffani07@gmail.com>
2022-10-08 15:31:50 -0400

    correction

--- area.js
+++ area.js
@@ -1,24 +1,24 @@
-function Area_of_circle(r){
-    let Area_of_circle = Math. PI * r*r
-    console.log(`Area of circle is ${Area_of_circle}`)
+function areaOfCircle(r){
+    let areaOfCircle = Math. PI * r*r
+    console.log(`Area of circle is ${areaOfCircle}`)
 }
-Area_of_circle(5)
+areaOfCircle(5)
 
-function Area_of_square(s){
-    let Area_of_square = 2*s
-    console.log(`Area of square is ${Area_of_square}`)
+function areaOfSquare(s){
+    let areaOfSquare = 2*s
+    console.log(`Area of square is ${areaOfSquare}`)
 }
-Area_of_square(10)
+areaOfSquare(10)
 
-function perimeter_of_suqare(s){
-    let perimeter_of_suqare = 4*s
-    console.log(`perimeter of square is ${perimeter_of_suqare}`)
+function perimeterOfSuqare(s){
+    let perimeterOfSuqare = 4*s
+    console.log(`perimeter of square is ${perimeterOfSuqare}`)
 }
-perimeter_of_suqare(6)
+perimeterOfSuqare(6)
 
 
-function Area_of_triangle(b,h){
-    let Area_of_triangle = 0.5*b*h
-    console.log(`Area of triangle is ${Area_of_triangle}`)
+function areaOfTriangle(b,h){
+    let areaOfTriangle = 0.5*b*h
+    console.log(`Area of triangle is ${areaOfTriangle}`)
 }
-Area_of_triangle(9,11)
+areaOfTriangle(9,11)
--- leapyear.js
+++ leapyear.js
@@ -3,7 +3,7 @@
 function leapYear(year)
 {
     let a = year
-    if (year % 4 == 0) {
+    if (year % 4 === 0) {
         console.log(`This year is leap year`)
     } else {
         console.log(`It is not an leap year`)
